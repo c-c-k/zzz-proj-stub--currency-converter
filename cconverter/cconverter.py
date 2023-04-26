949d42a91fa417e393ff89e250eafdd6eb9a1626
c-c-k <c-c-k@nym.hush.com>
2023-04-26 18:17:28 +0300

    stage 3 done

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -1,9 +1,9 @@
 def main():
+    print("Please, enter the number of conicoins you have:")
     conicoins = int(input())
-    print(f"I have {conicoins} conicoins.")
-    print(f"{conicoins} conicoins cost {conicoins * 100} dollars.")
-    print("I am rich! Yippee!")
-
+    print("Please, enter the exchange rate:")
+    exchange_rate = float(input())
+    print(f"The total amount of dollars: {conicoins * exchange_rate}")
 
 if __name__ == "__main__":
     main()
