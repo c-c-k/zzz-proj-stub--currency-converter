5d1ec6f26c3f6a3b2c70b6ced5e489ec1457050b
c-c-k <c-c-k@nym.hush.com>
2023-04-26 18:17:28 +0300

    stage 5 done

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -1,17 +1,12 @@
-EXCHANGE_RATES = {
-    "RUB": 2.98,  # Russian Ruble; 1 conicoin = 2.98 RUB;
-    "ARS": 0.82,  # Argentine Peso; 1 conicoin = 0.82 ARS;
-    "HNL": 0.17,  # Honduran Lempira; 1 conicoin = 0.17 HNL;
-    "AUD": 1.9622,  # Australian Dollar; 1 conicoin = 1.9622 AUD;
-    "MAD": 0.208,  # Moroccan Dirham; 1 conicoin = 0.208 MAD.
-}
+import requests
 
 
 def main():
-    conicoins = float(input())
-    for (cur_id, cur_rate) in EXCHANGE_RATES.items():
-        print(f"I will get {conicoins * cur_rate:.2f} {cur_id}"
-              f" from the sale of {conicoins} conicoins.")
+    cur_code = input()
+    url = f"http://www.floatrates.com/daily/{cur_code}.json"
+    exchange_rates = requests.get(url).json()
+    print(exchange_rates['usd'])
+    print(exchange_rates['eur'])
 
 
 if __name__ == "__main__":
