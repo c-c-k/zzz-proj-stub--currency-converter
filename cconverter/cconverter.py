b960f2fe4e13e5f114fc362059d9fa8da2ed5197
c-c-k <c-c-k@nym.hush.com>
2023-04-26 18:17:28 +0300

    stage 6 done

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -2,11 +2,32 @@ import requests
 
 
 def main():
-    cur_code = input()
-    url = f"http://www.floatrates.com/daily/{cur_code}.json"
-    exchange_rates = requests.get(url).json()
-    print(exchange_rates['usd'])
-    print(exchange_rates['eur'])
+    input_cur_code = input()
+
+    url = f"http://www.floatrates.com/daily/{input_cur_code}.json"
+    all_cur = requests.get(url, timeout=1).json()
+
+    cache = {cache_cur["code"].lower(): cache_cur["rate"] for cache_cur
+             in (cur_info for (cur_code, cur_info) in all_cur.items()
+                 if cur_code.lower() in {'usd', 'eur'})}
+
+    while True:
+        output_cur_code = input()
+        if output_cur_code == "":
+            break
+        cur_amount = input()
+        if cur_amount == "":
+            break
+        cur_amount = float(cur_amount)
+        print("Checking the cache...")
+        if output_cur_code.lower() in cache.keys():
+            print("Oh! It is in the cache!")
+        else:
+            print("Sorry, but it is not in the cache!")
+            base_cur = all_cur[output_cur_code.lower()]
+            cache[output_cur_code.lower()] = base_cur["rate"]
+        rate = cache[output_cur_code.lower()]
+        print(f"You received {cur_amount * rate:.2f} {output_cur_code}.")
 
 
 if __name__ == "__main__":
