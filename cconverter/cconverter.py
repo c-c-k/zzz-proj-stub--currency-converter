3b2b07694b255d7cae4ee0ffbf227b74f22231b5
c-c-k <c-c-k@nym.hush.com>
2023-04-26 18:17:28 +0300

    stage 2 done

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -1,6 +1,8 @@
 def main():
-    print("Meet a conicoin!")
-
+    conicoins = int(input())
+    print(f"I have {conicoins} conicoins.")
+    print(f"{conicoins} conicoins cost {conicoins * 100} dollars.")
+    print("I am rich! Yippee!")
 
 
 if __name__ == "__main__":
