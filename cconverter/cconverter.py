888879ba9e26d23a0b2190b839ba8a2797872a2c
c-c-k <c-c-k@nym.hush.com>
2023-04-26 18:17:28 +0300

    stage 4 done

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -1,9 +1,18 @@
+EXCHANGE_RATES = {
+    "RUB": 2.98,  # Russian Ruble; 1 conicoin = 2.98 RUB;
+    "ARS": 0.82,  # Argentine Peso; 1 conicoin = 0.82 ARS;
+    "HNL": 0.17,  # Honduran Lempira; 1 conicoin = 0.17 HNL;
+    "AUD": 1.9622,  # Australian Dollar; 1 conicoin = 1.9622 AUD;
+    "MAD": 0.208,  # Moroccan Dirham; 1 conicoin = 0.208 MAD.
+}
+
+
 def main():
-    print("Please, enter the number of conicoins you have:")
-    conicoins = int(input())
-    print("Please, enter the exchange rate:")
-    exchange_rate = float(input())
-    print(f"The total amount of dollars: {conicoins * exchange_rate}")
+    conicoins = float(input())
+    for (cur_id, cur_rate) in EXCHANGE_RATES.items():
+        print(f"I will get {conicoins * cur_rate:.2f} {cur_id}"
+              f" from the sale of {conicoins} conicoins.")
+
 
 if __name__ == "__main__":
     main()
